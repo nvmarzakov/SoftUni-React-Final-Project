660c344bc302f11c9892c107eb4fb0f36ea990dc
nvmarzakov <nikolay.marzakov@gmail.com>
2023-12-05 13:01:59 +0200

    update navigation component

--- e-commerce-shop/client/src/components/Navigation.jsx
+++ e-commerce-shop/client/src/components/Navigation.jsx
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom'
 
 export default function Navigation() {
     return (
-        <nav className="main-nav">
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/register'>Register</Link></li>
-                <li><Link to='/account'>Account</Link></li>
-                <li><Link to='/logout'>Logout</Link></li>
-                <li><Link to='/community'>Community</Link></li>
-            </ul>
-        </nav>
+        <>
+            <p class="site-title">
+                <Link to='/'>Recharge</Link>
+            </p>
+            <nav className="main-nav">
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/register'>Register</Link></li>
+                    <li><Link to='/account'>Account</Link></li>
+                    <li><Link to='/logout'>Logout</Link></li>
+                    <li><Link to='/community'>Community</Link></li>
+                </ul>
+            </nav>
+        </>
     )
 }
