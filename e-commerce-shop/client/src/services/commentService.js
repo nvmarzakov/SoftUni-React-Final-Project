12f3486034ee48859d7e5be5a8b6beac80d4c026
nvmarzakov <nikolay.marzakov@gmail.com>
2023-12-18 23:47:15 +0200

    filter comments
    have to fix to not get all comments

--- e-commerce-shop/client/src/services/commentService.js
+++ e-commerce-shop/client/src/services/commentService.js
@@ -3,10 +3,10 @@ import * as request from "../lib/request";
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
 
-export const getAll = async () => {
+export const getAll = async (exerciseId) => {
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    return Object.values(result).filter(comment => comment.exerciseId === exerciseId); //to fix 
 };
 
 export const create = async (exerciseId, username, text) => {
